2b8e9379cf1daf3b8c4e4ae94fede4308f40d338
Laksh Arora <laksharora28@gmail.com>
2016-06-28 23:57:05 +0530

    added cost function

--- optimization.py
+++ optimization.py
@@ -32,3 +32,39 @@ def printschedule(r):
 		out = flights[(origin, dest)][r[d*2]]
 		ret = flights[(dest, origin)][r[d*2+1]]
 		print '%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % (name, location[origin], out[0], out[1], out[2], ret[0], ret[1], ret[2])
+
+def schedulecost(sol):
+	totalprice = 0
+	latestarrival = 0
+	earliestdep = 24*60
+
+	for d in range(len(sol)/2):
+		# Get the inbound and outbound flights
+		origin = people[d][1]
+		outbound = flights[(origin, destination)][int(sol[d])]
+		returnf = flights[(destination, origin)][int(sol[d+1])]
+
+		# Total price is the price of all outbound and reutrn flights
+		totalprice += outbound[2]
+		totalprice += returnf[2]
+
+		# Track the latest arrival and earliest departure
+		if latestarrival < getminutes(outbound[1]):
+			latestarrival = getminutes(outbound[1])
+		if earliestdep > getminutes(returnf[0]):
+			earliestdep = getminutes(returnf[0])
+
+		# Every person must wait at the airport until the latest person arrives.
+		totalwait = 0
+		for d in range(len(sol)/2):
+			origin = people[d][1]
+			outbound = flights[(origin, destination)][int(sol[d])]
+			returnf = flights[(destination, origin)][int(sol[d+1])]
+			totalwait += latestarrival - getminutes(outbound[1])
+			totalwait += getminutes(returnf[0]) - earliestdep
+
+		# Extra day of car rental? Extra $50
+		if latestarrival > earliestdep:
+			totalprice += 50
+
+		return totalprice + totalwait
